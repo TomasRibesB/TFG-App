e0e04dc08b22a68f92282fa9669276778621fb60
Tomas Ribes <tomasribesb@gmail.com>
2024-10-11 14:06:23 -0300

    icons

--- src/presentation/navigation/BotTabNavigator.tsx
+++ src/presentation/navigation/BotTabNavigator.tsx
@@ -1,12 +1,14 @@
+import React from 'react';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { HomeScreen } from '../screens/HomeScreen';
 import { TopTabEntrenamiento } from './TopTabEntrenamiento';
 import { MD3DarkTheme, MD3LightTheme } from 'react-native-paper';
 import { useColorScheme } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons'; // Importa Ionicons
 
 export type RootTabParams = {
-    Home: undefined;
-    Entrenamiento: undefined;
+  Home: undefined;
+  Entrenamiento: undefined;
 };
 
 const Tab = createMaterialBottomTabNavigator<RootTabParams>();
@@ -16,21 +18,48 @@ export const BotTabNavigator = () => {
 
   return (
     <Tab.Navigator
-      initialRouteName='Home'
+      initialRouteName="Home"
       barStyle={{
         backgroundColor: isDarkMode
           ? MD3DarkTheme.colors.background
           : MD3LightTheme.colors.background,
         borderColor: isDarkMode
-            ? MD3DarkTheme.colors.surfaceVariant
-            : MD3LightTheme.colors.surfaceVariant,
+          ? MD3DarkTheme.colors.surfaceVariant
+          : MD3LightTheme.colors.surfaceVariant,
         borderTopWidth: 1,
       }}
       activeColor={isDarkMode ? MD3DarkTheme.colors.primary : MD3LightTheme.colors.primary}
       inactiveColor={isDarkMode ? MD3DarkTheme.colors.onSurface : MD3LightTheme.colors.onSurface}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, focused }) => {
+          let iconName: string = '';
+
+          // Asignación del nombre del icono
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Entrenamiento') {
+            iconName = focused ? 'barbell' : 'barbell-outline';
+          }
+
+          // Devuelve el icono de Ionicons
+          return <Ionicons name={iconName} size={24} color={color} />;
+        },
+      })}
     >
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Entrenamiento" component={TopTabEntrenamiento} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Inicio',
+        }}
+      />
+      <Tab.Screen
+        name="Entrenamiento"
+        component={TopTabEntrenamiento}
+        options={{
+          tabBarLabel: 'Entrenamiento',
+        }}
+      />
     </Tab.Navigator>
   );
-}
+};
